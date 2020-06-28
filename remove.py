4a39197eabddc95a256db0d17cc5f910d2c67c0a
Ab <abhisingh3174@gmail.com>
2020-06-28 17:54:00 +0530

    remove

---
+++ remove.py
@@ -0,0 +1,10 @@
+s=eval(input("Enter a list-"))
+s.remove(s[2])
+print(s)
+
+print(s.pop(1))
+print(s)
+print(s.pop())
+
+q=list("hello")
+print(q)
---
+++ replace.py
@@ -0,0 +1,5 @@
+s=input("Enter a string-")
+q=input("Enter replace string-")
+p=input("Enter a substring-")
+s=s.replace(q,p)
+print(s)
